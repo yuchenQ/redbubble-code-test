ac906eb60d52dce2459d823c21a391dd9048f12f
Redbubble Applicant <me@example.com>
2020-03-04 02:41:09 +1100

    build: remove useless loaders in webpack

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -52,43 +52,6 @@ const baseConfig = {
         ],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        include: path.join(SRC_DIRECTORY, 'assets'),
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/[name].[ext]',
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75,
-              },
-            },
-          },
-        ],
-      },
-      {
         test: /\.md$/,
         use: [
           {
